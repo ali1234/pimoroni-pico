abb17d28ace604ce51017cdc475d1eb7eba4ee3a
Mike Bell <mdb036@gmail.com>
2023-02-03 11:16:22 +0000

    MicroPython cmake nonsense

--- micropython/modules/pico_camera/micropython.cmake
+++ micropython/modules/pico_camera/micropython.cmake
@@ -6,16 +6,22 @@ target_sources(usermod_${MOD_NAME} INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/${MOD_NAME}.c
     ${CMAKE_CURRENT_LIST_DIR}/${MOD_NAME}.cpp
     ${CMAKE_CURRENT_LIST_DIR}/../../../libraries/pico_camera/pico_camera.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../../../drivers/aps6404/aps6404.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../../../drivers/ov2640/ov2640.cpp
 )
+
+pico_generate_pio_header(usermod_${MOD_NAME} ${CMAKE_CURRENT_LIST_DIR}/../../../drivers/aps6404/aps6404.pio)
+pico_generate_pio_header(usermod_${MOD_NAME} ${CMAKE_CURRENT_LIST_DIR}/../../../drivers/ov2640/ov2640.pio)
 
 target_include_directories(usermod_${MOD_NAME} INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
     ${CMAKE_CURRENT_LIST_DIR}/../../../libraries/pico_camera/
+    ${CMAKE_CURRENT_LIST_DIR}/../../../drivers/aps6404/
+    ${CMAKE_CURRENT_LIST_DIR}/../../../drivers/ov2640/
 )
 
 target_compile_definitions(usermod_${MOD_NAME} INTERFACE
     MODULE_PICOCAMERA_ENABLED=1
 )
 
-target_link_libraries(usermod INTERFACE usermod_${MOD_NAME}
-  pimoroni_i2c ov2640 aps6404)
+target_link_libraries(usermod INTERFACE usermod_${MOD_NAME})
